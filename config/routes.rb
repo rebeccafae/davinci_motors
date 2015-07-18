0746786bc9c275bcd178acd70884cc839d740fe4
Rebecca <rfjackson11@gmail.com>
2015-07-18 15:53:17 -0600

    Add tests to show list of cars and edit cars
    [#99348694]

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # get '/' => 'cars#index', as: 'root'
   root 'cars#index'
 
-  get 'cars/new' => 'cars#new', as: 'new_car'
+  # get 'cars/new' => 'cars#new', as: 'new_car'
 
-  post 'cars' => 'cars#create'
+  # post 'cars' => 'cars#create'
 
+  # get 'cars/edit' => 'cars#edit', as: 'edit_car'
+
+  resources :cars
 end
--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -13,6 +13,70 @@ feature 'Creating Cars' do
 
     click_button 'Create Car'
 
-    expect(page).to have_content('1968 Ford Mustang has been created')
+    expect(find(".flash")).to have_content('1968 Ford Mustang has been created')
+
+    expect(page).to have_table("car_table")
+
+    expect(find("#car_table")).to have_content("Ford")
+    expect(find("#car_table")).to have_content("Mustang")
+    expect(find("#car_table")).to have_content("1968")
+    expect(find("#car_table")).to have_content("2300")
+  end
+
+  scenario 'can add a second car' do
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1968'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Create Car'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Subaru'
+    fill_in 'Model', with: 'Legacy'
+    fill_in 'Year', with: '2005'
+    fill_in 'Price', with: '8000'
+
+    click_button 'Create Car'
+
+    expect(page).to have_table("car_table")
+
+    expect(find("#car_table")).to have_content("Ford")
+    expect(find("#car_table")).to have_content("Subaru")
+  end
+end
+
+feature 'Editing Cars' do
+  scenario 'can edit a car' do
+
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1968'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Create Car'
+
+    expect(find(".flash")).to have_content('1968 Ford Mustang has been created')
+
+    click_link 'Edit'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1950'
+    fill_in 'Price', with: '4000'
+
+    click_button 'Update Car'
+
+    expect(find(".flash")).to have_content('1950 Ford Mustang has been updated')
+
   end
 end
